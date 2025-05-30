13fbe6cd282a611b356e140ec55d7d9542a53e42
Will Cain <151270872+candycain5432@users.noreply.github.com>
2025-05-30 10:52:53 -0400

    Update get-products.js

--- netlify/functions/get-products.js
+++ netlify/functions/get-products.js
@@ -1,1 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+  const token = process.env.PRINTIFY_TOKEN;
+  const shopId = process.env.SHOP_ID;
+
+  const res = await fetch(`https://api.printify.com/v1/shops/${shopId}/products.json`, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+};
 
